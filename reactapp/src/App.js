ea786a843d91d4693cb429b7d9e0e7a5f2fa38cb
chndu07 <63121503+chndu07@users.noreply.github.com>
2022-04-07 20:24:51 +0530

    Add files via upload

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,102 +1,12 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import { Switch, Route } from "react-router-dom";
-import Carousel from "react-elastic-carousel";
-import Item from "./Item";
-import "./styles.css";
-import pic1 from "./images/glow.jpg";
-import pic2 from "./images/200106571A3334.jpg";
-import pic3 from "./images/glow.jpg";
-import pic4 from "./images/glow.jpg";
+import "./App.css";
+import ValidationInReact from "./ValidationInReact";
 
-const breakPoints = [
-  { width: 1, itemsToShow: 1 },
-  { width: 550, itemsToShow: 2 },
-  { width: 768, itemsToShow: 3 },
-  { width: 1200, itemsToShow: 4 },
-];
-
-const Home = () => {
+export default function App() {
   return (
-    <>
-      <Navbar />
-      <section className="hero-section">
-        <p>Welcome to </p>
-        <h1>Glow Spark Homepage</h1>
-        </section>
-        <div className="App">
-        <Carousel breakPoints={breakPoints}>
-          <Item><img className="photo" src={pic1}></img></Item>
-          <Item><img className="photo" src={pic2}></img></Item>
-          <Item>Three</Item>
-          <Item>Four</Item>
-          <Item>Five</Item>
-          <Item>Six</Item>
-          <Item>Seven</Item>
-          <Item>Eight</Item>
-        </Carousel>
-      </div>
-      
-    </>
+    <div className="App">
+      <h1>Appointment Booking</h1>
+      <ValidationInReact />
+    </div>
   );
-};
-
-const About = () => {
-  return (
-    <>
-      <Navbar />
-      <section className="hero-section">
-        <p>Welcome to </p>
-        <h1>Glow Spark About Page</h1>
-      </section>
-    </>
-  );
-};
-
-const Service = () => {
-  return (
-    <>
-      <Navbar />
-      <section className="hero-section">
-        <p>Welcome to </p>
-        <h1>Glow Spark Service Page</h1>
-      </section>
-    </>
-  );
-};
-
-const Contact = () => {
-  return (
-    <>
-      <Navbar />
-      <section className="hero-section">
-        <p>Welcome to </p>
-        <h1>Glow Spark Contact Page</h1>
-      </section>
-    </>
-  );
-};
-
-const App = () => {
-  return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-
-      <Route path="/about">
-        <About />
-      </Route>
-
-      <Route path="/service">
-        <Service />
-      </Route>
-
-      <Route path="/contact">
-        <Contact />
-      </Route>
-    </Switch>
-  );
-};
-
-export default App;
+}
